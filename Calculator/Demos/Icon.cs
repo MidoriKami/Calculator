4ad9b12e27699cbcf35058ead6344197a956edc3
Haselnussbomber <mail@haselnussbomber.de>
2025-07-13 13:37:40 +0200

    Update DragDropNode

--- Calculator/Demos/Icon.cs
+++ Calculator/Demos/Icon.cs
@@ -1,5 +1,8 @@
 ﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
+using KamiToolKit.Extensions;
 using KamiToolKit.Nodes;
 
 namespace Calculator.Demos;
@@ -10,18 +13,18 @@ public static class Icons {
 		AlignmentFlags = FlexFlags.FitContentHeight | FlexFlags.CenterVertically | FlexFlags.CenterHorizontally,
 		IsVisible = true,
 	};
-	
+
 	public static void IconDemo(TreeListCategoryNode treeListCategoryNode) {
 		treeListCategoryNode.AddHeader("Icon");
 		Icon(treeListCategoryNode);
-		
+
 		treeListCategoryNode.AddHeader("DragDropIcon");
 		DragDropIcon(treeListCategoryNode);
 	}
 
 	public static void Icon(TreeListCategoryNode treeListCategoryNode) {
 		var flexGrid = GetContainer(treeListCategoryNode);
-		
+
 		// IconNode's are complex composite nodes involving a **lot** of data from the game
 		// These represent interactable icons that the game shows
 		// WARNING: WORK IN PROGRESS, INTERACTIONS MAY NOT BEHAVE AS EXPECTED
@@ -46,28 +49,70 @@ public static class Icons {
 				iconNode.IsIconDisabled = !iconNode.IsIconDisabled;
 			},
 		};
-		
+
 		flexGrid.AddNode(iconNode);
 		flexGrid.AddNode(button);
-		
+
 		treeListCategoryNode.AddNode(flexGrid);
 	}
-	
+
 	private static void DragDropIcon(TreeListCategoryNode treeListCategoryNode) {
 		var flexGrid = GetContainer(treeListCategoryNode);
 
 		// DragDropNode's are even more complex nodes for accepting and providing actions and icons
-		// WARNING: WORK IN PROGRESS, INTERACTIONS MAY NOT BEHAVE AS EXPECTED
 		var dragDropNode = new DragDropNode {
-			X = 20.0f, 
-			Size = new Vector2(44.0f, 44.0f), 
-			IsVisible = true, 
-			IconId = 2551,
+			X = 20.0f,
+			Size = new Vector2(44.0f, 44.0f),
+			IsVisible = true,
+			IconId = 2, // Inventory
+			Payload = new DragDropPayload {
+				Type = DragDropType.MainCommand,
+				Int2 = 10,
+			},
+			IsClickable = true,
+			OnBegin = (node, data) => {
+				Serilog.Log.Debug("[DragDropNode] DragDrop started");
+			},
+			OnEnd = (node, data) => {
+				Serilog.Log.Debug("[DragDropNode] DragDrop ended");
+			},
+			OnPayloadAccepted = (node, data, payload) => {
+				Serilog.Log.Debug("[DragDropNode] Payload Accepted: {type} {int1} {int2}", payload.Type, payload.Int1, payload.Int2);
+			},
+			OnPayloadRejected = (node, data, payload) => {
+				Serilog.Log.Debug("[DragDropNode] Payload Rejected: {type} {int1} {int2}", payload.Type, payload.Int1, payload.Int2);
+			},
+			OnDiscard = (node, data) => {
+				Serilog.Log.Debug("[DragDropNode] Payload Discarded");
+			},
+			OnClicked = (node, data) => {
+				var dragDropData = data.GetDragDropData();
+
+				switch(dragDropData.MouseButtonId) {
+				case 0:
+					Serilog.Log.Debug("[DragDropNode] Clicked left mouse button");
+
+					unsafe {
+						// just for verbosity. you could also just call ExecuteMainCommand(10) directly
+						switch(dragDropData.DragDropInterface->DragDropType) {
+						case DragDropType.MainCommand:
+							UIModule.Instance()->ExecuteMainCommand((uint)dragDropData.DragDropInterface->GetPayloadContainer()->Int2);
+							break;
+						}
+					}
+					break;
+
+				case 1:
+					Serilog.Log.Debug("[DragDropNode] Clicked right mouse button");
+					// you could open a context menu here
+					break;
+				}
+			}
 		};
-		
+
 		flexGrid.AddNode(dragDropNode);
 		flexGrid.AddDummy(new ResNode(), 0.0f);
-		
+
 		treeListCategoryNode.AddNode(flexGrid);
 	}
 }
